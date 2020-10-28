371c914d6b905e403b0cc932ffb6f2ff34efc68d
Renda02 <rendaniluvhengo89@gmail.com>
2020-10-28 07:33:33 +0100

    Added Footer

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/*Navbar*/}
+      {/*TopSection*/}
+      {/*Body */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <div id="sections-container">
+        <div className="footer-column">
+          <h3 className="footer-section-title">Rendani Luvhengo</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.{" "}
+          </p>
+        </div>
+        <div className="footer-column">
+          <h3 className="footer-section-title">Links</h3>
+          <a></a>
+        </div>
+        <div className="footer-column">
+          <h3 className="footer-section-title">Follow me</h3>
+        </div>
+      </div>
+      <div className="footer-copy">
+        <p>Made with love in Huelva. Copyright @Rendani 2020</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
