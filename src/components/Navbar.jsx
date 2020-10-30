daf05e8a5dfce647dce95432cb69982f3df27bcf
Renda02 <rendaniluvhengo89@gmail.com>
2020-10-30 09:09:43 +0100

    Styled Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@
 function Navbar() {
     return (
         <div className="header">
-          
-                <h4 className="logo">Rendani Luvhengo</h4>  <nav>
+           <nav className="navbar">
+                <h4 className="logo">Rendani Luvhengo</h4> 
             <ul className="nav__list">
                 <li><a href="/">Home</a></li>
                 <li><a href="/">About</a> </li>
                 <li><a  href="/" className ="navbar__contact">Contact</a></li>
-                
+              
             </ul></nav>
-            <input type="search"></input>
+           
             
             
         </div>
