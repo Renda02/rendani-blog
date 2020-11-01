4c6c3b1be1a374f55a9b65bbebb2b4267222dd2b
Renda02 <rendaniluvhengo89@gmail.com>
2020-11-01 09:39:10 +0100

    Added github calendar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,38 @@
-
+import GitHubCalendar from "react-github-calendar";
+import ReactTooltip from "react-tooltip";
 
 function Main() {
+
+const exampleTheme = {
+  background: "transparent",
+  text: "#000",
+  grade4: "hsl(338, 78%, 30%)",
+  grade3: "hsl(338, 78%, 44%)",
+  grade2: "hsl(338, 78%, 58%)",
+  grade1: "hsl(338, 78%, 72%)",
+  grade0: "#eee",
+};
+
     return (
-        <div className="main">
-            <div className="main__heading">
-                <h2> What have I been to</h2>
-                <h3>I come from manufacturing and construstion industry where quality-work,continuous improvement and meeting all customer's requirements is a priority. But in the end, my fascination with tech inspired me take the leap.</h3>
-                
-            </div>
-            
+      <div className="main">
+        <div className="main__heading">
+          <h2> What have I been to</h2>
+          <h3>
+            I come from manufacturing and construstion industry where
+            quality-work,continuous improvement and meeting all customer's
+            requirements is a priority. But in the end, my fascination with tech
+            inspired me take the leap.
+          </h3>
+          <br />
         </div>
-    )
+        <div className="calendar">
+          <h3>GitHub Calendar</h3>
+          <GitHubCalendar username="renda02" theme={exampleTheme}>
+            <ReactTooltip delayShow={50} html />
+          </GitHubCalendar>
+        </div>
+      </div>
+    );
 }
 
 export default Main
