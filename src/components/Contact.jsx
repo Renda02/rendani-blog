865ee2268fb9a376872efa8c559060661c7c0a87
Renda02 <rendaniluvhengo89@gmail.com>
2020-11-04 06:17:48 +0100

    added styled components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,21 +2,53 @@ function Contact() {
   return (
     <div className="form">
       <h2 className="form__heading">Contact me</h2>
-      <form action="">
-        <div className="input__name">
-          <input type="text" />
-          <label>First Name</label>
-          <input type="text" />
-          <label>Last Name</label>
-        </div>
-          <div className="input__data">
-            <input type="email" />
-            <label>Email Adress</label>
+      <div className="form__row">
+        <div className="col">
+          <div className="form__input">
+            <input type="text" name="" required="required" />
+            <span className="text">First Name</span>
+            <span className="line"></span>
           </div>
-       
-        <br />
-        <input type="submit" />
-      </form>
+        </div>
+
+        <div className="col">
+          <div className="form__input">
+            <input type="text" name="" required="required" />
+            <span className="text">Last Name</span>
+            <span className="line"></span>
+          </div>
+        </div>
+      </div>
+      <div className="form__row">
+        <div className="col">
+          <div className="form__input">
+            <input type="text" name="" required="required" />
+            <span className="text">Email</span>
+            <span className="line"></span>
+          </div>
+        </div>
+        <div className="col">
+          <div className="form__input">
+            <input type="text" name="" required="required" />
+            <span className="text">Mobile Number</span>
+            <span className="line"></span>
+          </div>
+        </div>
+      </div>
+      <div className="form__row">
+        <div className="col">
+          <div className="form__input textarea">
+            <textarea required="required"></textarea>
+            <span className="text">Type your message here..</span>
+            <span className="line"></span>
+          </div>
+        </div>
+      </div>
+      <div className="form__row">
+        <div className="col">
+          <input type="submit" value="Send"/>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,21 +1,34 @@
-import Typed from 'react-typed';
-import avatar from "../images/image.JPG"
+import Typed from "react-typed";
+import avatar from "../images/image.JPG";
+import styled from "styled-components"
+
+const IntroContainer = styled.div`
+  text-align: center;
+  line-height: 24px;
+  padding-top: 200px;
+  padding-bottom: 175px;
+  display: flex-start;
+`;
+
 
 function TopSection() {
-    return (
-        <div className="top__section">
-            <div className="intro">
-                <p>Hello , I am</p>
-                <h1>Rendani Luvhengo</h1>
-                <br/>
-            <p className="title"><Typed strings={["Front End Developer"]} typeSpeed={30} showCursor={false} /></p> 
-
-            </div>
-            <img src={avatar} alt="logo" />
-           
-            
-        </div>
-    )
+  return (
+    <div className="top__section">
+      <IntroContainer>
+        <p>Hello , I am</p>
+        <h1>Rendani Luvhengo</h1>
+        <br />
+        <p className="title">
+          <Typed
+            strings={["Front End Developer"]}
+            typeSpeed={30}
+            showCursor={false}
+          />
+        </p>
+      </IntroContainer>
+      <img src={avatar} alt="logo" />
+    </div>
+  );
 }
 
-export default TopSection
+export default TopSection;
